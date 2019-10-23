d5ac9ea2e6b34310abc2c20bf1a6ce7808556783
andersondiassiqueira33@outlook.com <andersondiassiqueira33@outlook.com>
2019-10-23 13:21:04 -0300

    cc

---
+++ custom/js/custom.js
@@ -0,0 +1,32 @@
+function toggleI() {
+    var x = document.getElementById("content");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+
+  }
+
+
+  function toggleII() {
+     var y = document.getElementById("content2")
+     if (y.style.display === "none") {
+        y.style.display = "block";
+    } else {
+        y.style.display = "none";
+    }
+
+  }
+ 
+
+  function toggleIII() {
+ 
+    var z = document.getElementById("content3")
+    if (z.style.display === "none") {
+        z.style.display = "block";
+    } else {
+        z.style.display = "none";
+    }
+
+  }
